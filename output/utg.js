b99eff0f962f8c3ad5b5da70ec958f9711d621e3
NhuY <congchuanhuy19@gmail.com>
2025-08-11 20:29:20 +0700

    Open DCIM in gallery result

---
+++ output/utg.js
@@ -0,0 +1,208 @@
+var utg = 
+{
+  "nodes": [
+    {
+      "id": "0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231627.jpg",
+      "label": "Launcher\n<FIRST>",
+      "package": "com.android.launcher3",
+      "activity": ".Launcher",
+      "state_str": "0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb",
+      "structure_str": "85847ecc1a733e06ee193298d1109cd3",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.android.launcher3</td></tr>\n<tr><th>activity</th><td>.Launcher</td></tr>\n<tr><th>state_str</th><td>0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb</td></tr>\n<tr><th>structure_str</th><td>85847ecc1a733e06ee193298d1109cd3</td></tr>\n</table>",
+      "content": "com.android.launcher3\n.Launcher\n0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb\ncom.google.android.googlequicksearchbox:id/search_widget_google_logo,com.android.launcher3:id/hotseat,com.android.launcher3:id/cling_content,com.android.launcher3:id/active,com.google.android.googlequicksearchbox:id/search_widget_voice_btn,com.android.launcher3:id/cling_dismiss_longpress_info,com.android.launcher3:id/drag_layer,com.android.launcher3:id/workspace,com.android.launcher3:id/page_indicator,com.android.launcher3:id/search_drop_target_bar,com.android.launcher3:id/launcher,com.android.launcher3:id/longpress_cling,com.android.launcher3:id/qsb_widget,com.google.android.googlequicksearchbox:id/search_plate_padding_frame,com.android.launcher3:id/layout,com.google.android.googlequicksearchbox:id/search_edit_frame,com.android.launcher3:id/focus_indicator,android:id/content,com.android.launcher3:id/inactive,com.google.android.googlequicksearchbox:id/search_plate\nCamera,Chrome,Wallpapers, widgets, & settings,Messenger,Welcome,Touch & hold background to customize,Gmail,Phone,Play Store,GOT IT",
+      "font": "14px Arial red"
+    },
+    {
+      "id": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231633.jpg",
+      "label": "MainActivity",
+      "package": "com.simplemobiletools.applauncher",
+      "activity": ".activities.MainActivity",
+      "state_str": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "structure_str": "0283cbd0ca13b27ba9555e89f2756065",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.simplemobiletools.applauncher</td></tr>\n<tr><th>activity</th><td>.activities.MainActivity</td></tr>\n<tr><th>state_str</th><td>150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218</td></tr>\n<tr><th>structure_str</th><td>0283cbd0ca13b27ba9555e89f2756065</td></tr>\n</table>",
+      "content": "com.simplemobiletools.applauncher\n.activities.MainActivity\n150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218\ncom.simplemobiletools.applauncher:id/top_app_bar_layout,com.simplemobiletools.applauncher:id/sort,com.simplemobiletools.applauncher:id/main_relative_layout,android:id/statusBarBackground,com.simplemobiletools.applauncher:id/top_toolbar_search,com.simplemobiletools.applauncher:id/toggle_app_name,com.simplemobiletools.applauncher:id/action_bar_root,com.simplemobiletools.applauncher:id/top_toolbar,com.simplemobiletools.applauncher:id/top_toolbar_search_icon,com.simplemobiletools.applauncher:id/top_toolbar_holder,android:id/navigationBarBackground,com.simplemobiletools.applauncher:id/fab,com.simplemobiletools.applauncher:id/launchers_holder,com.simplemobiletools.applauncher:id/main_menu,com.simplemobiletools.applauncher:id/no_items_placeholder,android:id/content,com.simplemobiletools.applauncher:id/no_items_placeholder_2,com.simplemobiletools.applauncher:id/main_coordinator,com.simplemobiletools.applauncher:id/coordinator_layout\nNo items found.,Search,Manage visible app icons"
+    },
+    {
+      "id": "ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231659.jpg",
+      "label": "MainActivity",
+      "package": "com.simplemobiletools.applauncher",
+      "activity": ".activities.MainActivity",
+      "state_str": "ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c",
+      "structure_str": "533885111bdc9d7a4ada021d9b2c92b6",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.simplemobiletools.applauncher</td></tr>\n<tr><th>activity</th><td>.activities.MainActivity</td></tr>\n<tr><th>state_str</th><td>ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c</td></tr>\n<tr><th>structure_str</th><td>533885111bdc9d7a4ada021d9b2c92b6</td></tr>\n</table>",
+      "content": "com.simplemobiletools.applauncher\n.activities.MainActivity\nca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c\ncom.simplemobiletools.applauncher:id/content,com.simplemobiletools.applauncher:id/title\nColumn count,About,Settings"
+    },
+    {
+      "id": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231708.jpg",
+      "label": "SettingsActivity",
+      "package": "com.simplemobiletools.applauncher",
+      "activity": ".activities.SettingsActivity",
+      "state_str": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "structure_str": "197b12a908c304c442d334f88e057a5f",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.simplemobiletools.applauncher</td></tr>\n<tr><th>activity</th><td>.activities.SettingsActivity</td></tr>\n<tr><th>state_str</th><td>c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7</td></tr>\n<tr><th>structure_str</th><td>197b12a908c304c442d334f88e057a5f</td></tr>\n</table>",
+      "content": "com.simplemobiletools.applauncher\n.activities.SettingsActivity\nc1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7\ncom.simplemobiletools.applauncher:id/settings_toolbar,com.simplemobiletools.applauncher:id/settings_close_app,com.simplemobiletools.applauncher:id/action_bar_root,com.simplemobiletools.applauncher:id/settings_coordinator,com.simplemobiletools.applauncher:id/settings_nested_scrollview,com.simplemobiletools.applauncher:id/settings_color_customization_divider,android:id/content,com.simplemobiletools.applauncher:id/settings_general_settings_label,com.simplemobiletools.applauncher:id/settings_close_app_holder,com.simplemobiletools.applauncher:id/settings_color_customization_section_label,com.simplemobiletools.applauncher:id/settings_color_customization_label,android:id/navigationBarBackground,android:id/statusBarBackground,com.simplemobiletools.applauncher:id/settings_holder,com.simplemobiletools.applauncher:id/settings_color_customization_holder\nCOLOR CUSTOMIZATION,Close this app at launching a different one,Customize colors,GENERAL,Settings"
+    },
+    {
+      "id": "06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231743.jpg",
+      "label": "CustomizationActivity",
+      "package": "com.simplemobiletools.applauncher",
+      "activity": "com.simplemobiletools.commons.activities.CustomizationActivity",
+      "state_str": "06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da",
+      "structure_str": "cd9ecaf354d4a4955063c0c8c8a43598",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.simplemobiletools.applauncher</td></tr>\n<tr><th>activity</th><td>com.simplemobiletools.commons.activities.CustomizationActivity</td></tr>\n<tr><th>state_str</th><td>06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da</td></tr>\n<tr><th>structure_str</th><td>cd9ecaf354d4a4955063c0c8c8a43598</td></tr>\n</table>",
+      "content": "com.simplemobiletools.applauncher\ncom.simplemobiletools.commons.activities.CustomizationActivity\n06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da\ncom.simplemobiletools.applauncher:id/customization_app_icon_color,com.simplemobiletools.applauncher:id/customization_text_color,android:id/statusBarBackground,com.simplemobiletools.applauncher:id/customization_background_color_label,com.simplemobiletools.applauncher:id/customization_theme_label,com.simplemobiletools.applauncher:id/action_bar_root,com.simplemobiletools.applauncher:id/customization_theme,com.simplemobiletools.applauncher:id/customization_text_color_holder,com.simplemobiletools.applauncher:id/customization_nested_scrollview,com.simplemobiletools.applauncher:id/customization_primary_color,android:id/navigationBarBackground,com.simplemobiletools.applauncher:id/customization_app_icon_color_holder,com.simplemobiletools.applauncher:id/customization_toolbar,com.simplemobiletools.applauncher:id/customization_background_color,com.simplemobiletools.applauncher:id/customization_background_color_holder,com.simplemobiletools.applauncher:id/customization_theme_holder,com.simplemobiletools.applauncher:id/customization_coordinator,com.simplemobiletools.applauncher:id/customization_holder,com.simplemobiletools.applauncher:id/customization_text_color_label,com.simplemobiletools.applauncher:id/customization_app_icon_color_label,com.simplemobiletools.applauncher:id/customization_primary_color_label,com.simplemobiletools.applauncher:id/customization_primary_color_holder,android:id/content\nTheme,Background color,Primary color,Text color,Customize colors,App icon color,Dark"
+    },
+    {
+      "id": "7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb",
+      "shape": "image",
+      "image": "states\\screen_2025-08-09_231852.jpg",
+      "label": "MainActivity\n<LAST>",
+      "package": "com.simplemobiletools.applauncher",
+      "activity": ".activities.MainActivity",
+      "state_str": "7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb",
+      "structure_str": "79b6097bd26180b287f6f23e08793cac",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.simplemobiletools.applauncher</td></tr>\n<tr><th>activity</th><td>.activities.MainActivity</td></tr>\n<tr><th>state_str</th><td>7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb</td></tr>\n<tr><th>structure_str</th><td>79b6097bd26180b287f6f23e08793cac</td></tr>\n</table>",
+      "content": "com.simplemobiletools.applauncher\n.activities.MainActivity\n7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb\ncom.simplemobiletools.applauncher:id/sorting_dialog_holder,com.simplemobiletools.applauncher:id/sorting_dialog_radio_custom,com.simplemobiletools.applauncher:id/customPanel,com.simplemobiletools.applauncher:id/action_bar_root,com.simplemobiletools.applauncher:id/sorting_dialog_radio_title,com.simplemobiletools.applauncher:id/custom,android:id/button1,com.simplemobiletools.applauncher:id/sorting_dialog_radio_descending,com.simplemobiletools.applauncher:id/sorting_dialog_radio_ascending,com.simplemobiletools.applauncher:id/topPanel,com.simplemobiletools.applauncher:id/parentPanel,com.simplemobiletools.applauncher:id/sorting_dialog_radio_sorting,android:id/button2,com.simplemobiletools.applauncher:id/dialog_title_textview,com.simplemobiletools.applauncher:id/sorting_dialog_scrollview,com.simplemobiletools.applauncher:id/sorting_dialog_radio_order,com.simplemobiletools.applauncher:id/buttonPanel,android:id/content,com.simplemobiletools.applauncher:id/sorting_dialog_divider\nCancel,Sort by,Descending,OK,Title,Custom,Ascending",
+      "font": "14px Arial red"
+    }
+  ],
+  "edges": [
+    {
+      "from": "0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb",
+      "to": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "id": "0ffdc3daac7f1c9166ef8f56b7c24429b3d7b14992e423bd4f6f583968a40deb-->150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "title": "<table class=\"table\">\n<tr><th>1</th><td>IntentEvent(intent='am start com.simplemobiletools.applauncher/com.simplemobiletools.applauncher.activities.SplashActivity.Orange')</td></tr>\n</table>",
+      "label": "1",
+      "events": [
+        {
+          "event_str": "IntentEvent(intent='am start com.simplemobiletools.applauncher/com.simplemobiletools.applauncher.activities.SplashActivity.Orange')",
+          "event_id": 1,
+          "event_type": "intent",
+          "view_images": []
+        }
+      ]
+    },
+    {
+      "from": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "to": "ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c",
+      "id": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218-->ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c",
+      "title": "<table class=\"table\">\n<tr><th>6</th><td>TouchEvent(state=150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218, view=cd32a3c1fa7f0424cb193fbe0c5e5708(MainActivity/ImageView-))</td></tr>\n</table>",
+      "label": "6",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218, view=cd32a3c1fa7f0424cb193fbe0c5e5708(MainActivity/ImageView-))",
+          "event_id": 6,
+          "event_type": "click",
+          "view_images": [
+            "views/view_cd32a3c1fa7f0424cb193fbe0c5e5708.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "to": "7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb",
+      "id": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218-->7bd9e2ded8ae3ec34dffeefb946dad82da73e3a8c7d983d8eed62b7ba2093beb",
+      "title": "<table class=\"table\">\n<tr><th>7</th><td>TouchEvent(state=150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218, view=73999f854f503b1c7484978811ae0a14(MainActivity/Button-))</td></tr>\n</table>",
+      "label": "7",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218, view=73999f854f503b1c7484978811ae0a14(MainActivity/Button-))",
+          "event_id": 7,
+          "event_type": "click",
+          "view_images": [
+            "views/view_73999f854f503b1c7484978811ae0a14.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c",
+      "to": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "id": "ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c-->c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "title": "<table class=\"table\">\n<tr><th>6</th><td>TouchEvent(state=ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c, view=aeaa4360f2c6cc25e6e151816f23e18a(MainActivity/TextView-Settings))</td></tr>\n</table>",
+      "label": "6",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=ca19303f69337e3b0304675f3593b6929a8f5e20f11c851c7caa0c01e2149f4c, view=aeaa4360f2c6cc25e6e151816f23e18a(MainActivity/TextView-Settings))",
+          "event_id": 6,
+          "event_type": "click",
+          "view_images": [
+            "views/view_aeaa4360f2c6cc25e6e151816f23e18a.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "to": "150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "id": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7-->150676a63a8d08e76cd51a09d2d0fcd30fba99be8db54ada4598b60652c6c218",
+      "title": "<table class=\"table\">\n<tr><th>6</th><td>KeyEvent(state=c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7, name=BACK)</td></tr>\n</table>",
+      "label": "6",
+      "events": [
+        {
+          "event_str": "KeyEvent(state=c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7, name=BACK)",
+          "event_id": 6,
+          "event_type": "key",
+          "view_images": []
+        }
+      ]
+    },
+    {
+      "from": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "to": "06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da",
+      "id": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7-->06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da",
+      "title": "<table class=\"table\">\n<tr><th>6</th><td>TouchEvent(state=c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7, view=3799199fea1e1153378bcf2d05dc4dfd(SettingsActivity/TextView-Customize ))</td></tr>\n</table>",
+      "label": "6",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7, view=3799199fea1e1153378bcf2d05dc4dfd(SettingsActivity/TextView-Customize ))",
+          "event_id": 6,
+          "event_type": "click",
+          "view_images": [
+            "views/view_3799199fea1e1153378bcf2d05dc4dfd.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da",
+      "to": "c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "id": "06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da-->c1b06bce7e23dd13a3288977fbdd55b3d1962a9bb93432a712321fd371f52bb7",
+      "title": "<table class=\"table\">\n<tr><th>6</th><td>KeyEvent(state=06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da, name=BACK)</td></tr>\n</table>",
+      "label": "6",
+      "events": [
+        {
+          "event_str": "KeyEvent(state=06d12a81a984d165f6107d52300085d901815170a2c6546fc7129a5ff30b88da, name=BACK)",
+          "event_id": 6,
+          "event_type": "key",
+          "view_images": []
+        }
+      ]
+    }
+  ],
+  "num_nodes": 6,
+  "num_edges": 7,
+  "num_effective_events": 7,
+  "num_reached_activities": 3,
+  "test_date": "2025-08-09 23:16:19",
+  "time_spent": 153.07761,
+  "num_transitions": 17,
+  "device_serial": "emulator-5554",
+  "device_model_number": "Android SDK built for x86",
+  "device_sdk_version": 24,
+  "app_sha256": "1f7a3ce835a3e4d5e1f4a93bfe8f2668ec96456485c9437d841150ed3579be8b",
+  "app_package": "com.simplemobiletools.applauncher",
+  "app_main_activity": "com.simplemobiletools.applauncher.activities.SplashActivity.Orange",
+  "app_num_total_activities": 8
+}
